5af80560404ae56a08599ace0d3a5a1203717fb1
Gustavo Mandolesi <gustavo.mandolesi@gmail.com>
2022-11-29 21:47:57 -0300

    Added JSDoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,21 +14,6 @@ module.exports = function (grunt) {
         dest: "lib/<%= pkg.name %>.js",
       },
     },
-    uglify: {
-      options: {
-        compress: {
-          global_defs: {
-            IS_MINIFIED: true
-          }
-        },        
-        banner:
-          '/*! <%= pkg.name %>.js v<%= pkg.version %> - <%= grunt.template.today("mmmm dd, yyyy") %> - License: <%= pkg.license %> */ '
-      },
-      build: {
-        src: "lib/<%= pkg.name %>.js",
-        dest: "lib/<%= pkg.name %>.min.js",
-      },
-    },
     copy: {
       main: {
         files: [
@@ -49,30 +34,47 @@ module.exports = function (grunt) {
             cwd: "lib/",
             src: ["p5.palette.js"],
             dest: "examples/public/lib",
-          },          
+          },
         ],
       },
     },
-	watch: {
-		quick: {
-		  files: [
-			'src/**/*.js',
-		  ],
-		  tasks: ['default'],
-		//   options: {
-		// 	livereload: true
-		//   }
-		},
-	},	
+    jsdoc: {
+      dist: {
+        src: ["src/*.js", "test/*.js"],
+        options: {
+          destination: "doc",
+        },
+      },
+    },
+    uglify: {
+      options: {
+        compress: {
+          global_defs: {
+            IS_MINIFIED: true,
+          },
+        },
+        banner: '/*! <%= pkg.name %>.js v<%= pkg.version %> - <%= grunt.template.today("mmmm dd, yyyy") %> - License: <%= pkg.license %> */ ',
+      },
+      build: {
+        src: "lib/<%= pkg.name %>.js",
+        dest: "lib/<%= pkg.name %>.min.js",
+      },
+    },    
+    watch: {
+      quick: {
+        files: ["src/**/*.js"],
+        tasks: ["default"],
+      },
+    },
   });
 
   // Load external libraries used.
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks("grunt-contrib-concat");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-jsdoc");
 
   // Default task(s).
-  grunt.registerTask("default", ["concat", "uglify", "copy"]);
+  grunt.registerTask("default", ["concat", "uglify", "copy", "jsdoc"]);
 };
