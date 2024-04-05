d40d8c2ab86f1ee326ad3a3449f140439402977c
Gustavo Mandolesi <gustavo.mandolesi@gmail.com>
2024-04-04 22:26:50 -0300

    Update swatch.js

--- src/swatch.js
+++ src/swatch.js
@@ -1,8 +1,25 @@
+/**
+ * Represents a single color swatch within a palette.
+ * Each swatch has a color and additional properties that
+ * determine its weighting for random selection and whether
+ * it should be skipped in certain operations.
+ *
+ * @class Swatch
+ */
 class Swatch {
-  constructor(color, weight, skip) {
-    if (!color) throw "A swatch needs a color!";
+  /**
+   * Creates an instance of Swatch.
+   *
+   * @constructor
+   * @param {p5.Color} color - The color associated with this swatch.
+   * @param {number} [weight=1] - The weight of the color, used for random selection.
+   * @param {boolean} [skip=false] - Whether to skip this color in certain operations.
+   * @throws Will throw an error if the `color` parameter is not provided.
+   */
+  constructor(color, weight = 1, skip = false) {
+    if (!color) throw new Error("A swatch needs a color!");
     this.color = color;
-    this.weight = weight || 1;
-    this.skip = skip || false;
+    this.weight = weight;
+    this.skip = skip;
   }
 }
